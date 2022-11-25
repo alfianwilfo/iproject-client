c242489d5a3ca7f07410b305533eb84489597b53
alfian <alfianwilfredohoris@gmail.com>
2022-11-25 15:19:05 +0700

    fetch data from api done

--- Client/src/stores/counter.js
+++ Client/src/stores/counter.js
@@ -1,7 +1,32 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
+import axios from "axios";
 
 export const useCounterStore = defineStore("counter", {
-  state: () => {},
-  actions: () => {},
+  state: () => {
+    movies: [];
+  },
+  actions: {
+    getMovie() {
+      axios({
+        url: "https://imdb-top-100-movies.p.rapidapi.com/premiummovies",
+        headers: {
+          "X-RapidAPI-Key":
+            "206048b294msh3f43529c347895ep13c30ajsn0d9f344e0ca0",
+          "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com",
+        },
+        method: "GET",
+      })
+        .then((obj) => {
+          let { data } = obj;
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally((ee) => {
+          console.log(ee, "????");
+        });
+    },
+  },
 });
--- Client/src/views/HomePage.vue
+++ Client/src/views/HomePage.vue
@@ -1,13 +1,25 @@
 <script>
 import NavBar from "../components/NavBar.vue";
+import Card from "../components/Card.vue";
+import { mapState, mapActions } from "pinia";
+import { useCounterStore } from "../stores/counter";
+
 export default {
   name: "HomePage",
   components: {
     NavBar,
+    Card,
+  },
+  methods: {
+    ...mapActions(useCounterStore, ["getMovie"]),
+  },
+  created() {
+    this.getMovie();
   },
 };
 </script>
 
 <template>
   <NavBar />
+  <Card />
 </template>
