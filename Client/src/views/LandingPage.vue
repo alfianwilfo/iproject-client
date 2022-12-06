a4f446da4c0bf84d261d26907edd20183f4a36a8
alfian <alfianwilfredohoris@gmail.com>
2022-12-06 14:57:10 +0700

    landing page with random pick movie image

--- Client/src/views/LandingPage.vue
+++ Client/src/views/LandingPage.vue
@@ -1,4 +1,5 @@
 <script>
+import Footer from "../components/Footer.vue";
 import { mapActions, mapState } from "pinia";
 import { useCounterStore } from "../stores/counter";
 import NavBar from "../components/NavBar.vue";
@@ -6,6 +7,7 @@ export default {
   name: "LandingPage",
   components: {
     NavBar,
+    Footer,
   },
   created() {
     this.getMovie();
@@ -13,7 +15,40 @@ export default {
   methods: {
     ...mapActions(useCounterStore, ["getMovie"]),
   },
+  computed: {
+    ...mapState(useCounterStore, ["random"]),
+  },
 };
 </script>
 
-<template><NavBar /></template>
+<template>
+  <div class="h-screen flex flex-col">
+    <div>
+      <NavBar />
+    </div>
+    <div>
+      <div class="flex justify-center items-center h-[38rem]">
+        <div class="text-center w-[43rem]">
+          <div>
+            <p class="text-[42px] font-bold">
+              100 Best Movies of All Time That You Should Watch Immediately
+            </p>
+          </div>
+          <div>
+            <router-link to="/top100">
+              <button
+                class="text-[29px] bg-[#497174] rounded-lg text-white w-[400px] h-[46px] hover:brightness-150"
+              >
+                Check them now
+              </button>
+            </router-link>
+          </div>
+        </div>
+        <div class="w-[230px]">
+          <img :src="random.image" alt="" />
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
