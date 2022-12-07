b88f7f9fca52603e69d9e980ae8759df151f2c53
alfian <alfianwilfredohoris@gmail.com>
2022-12-07 12:44:52 +0700

    init register

--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -4,6 +4,7 @@ import Home from "../views/ListCountry.vue";
 import Detail from "../views/Detail.vue";
 import Hospital from "../views/Hospital.vue";
 import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: "/login",
       name: "login",
       component: Login,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
     },
   ],
 });
---
+++ Client/src/views/Login.vue
@@ -0,0 +1,7 @@
+<script>
+export default {
+  name: "login",
+};
+</script>
+
+<template></template>
